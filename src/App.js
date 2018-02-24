aeb6b1a733a46c8fc2a9009f60198ba90704fb44
Blair Lundin <blair.lundin@gmail.com>
2018-02-24 13:28:47 +1100

    Testing CI

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          React app goes here.
+          React app goes here!
         </p>
       </div>
     );
