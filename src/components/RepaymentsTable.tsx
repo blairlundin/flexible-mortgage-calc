e6305b036abc85c9288fed4744991bf594ecb2fa
Blair Lundin <blair.lundin@gmail.com>
2018-02-28 19:49:47 +1100

    Added a basic chart

--- src/components/RepaymentsTable.tsx
+++ src/components/RepaymentsTable.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Mortgage } from '../logic/MortgageCalculator';
 import { Table } from 'reactstrap';
+import { Line } from 'react-chartjs-2';
 
 let mortgageCalc = new Mortgage.MortgageCalculator();
 
@@ -86,20 +87,50 @@ export default class RepaymentsTable extends React.Component<any, any> {
             );
         });
 
+        var chartData = {
+            labels: annualPayments.map(a => a.year.toString(10)),
+            datasets: [
+              {
+                label: 'Total amount owing',
+                fill: true,
+                lineTension: 0.1,
+                backgroundColor: 'rgba(75,192,192,0.4)',
+                borderColor: 'rgba(75,192,192,1)',
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: 'rgba(75,192,192,1)',
+                pointBackgroundColor: '#fff',
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                pointHoverBorderColor: 'rgba(220,220,220,1)',
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: annualPayments.map(a => parseFloat(a.totalOwing.toFixed(2)))
+              }
+            ]
+        };
+
         return (
-            <Table className="table-sm">
-                <thead>
-                    <tr>
-                        <th>Year</th>
-                        <th>Interest</th>
-                        <th>Principal</th>
-                        <th>Owing</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {tableRows}
-                </tbody>
-            </Table>
+            <div>
+                <Line data={chartData} />
+                <Table className="table-sm mt-5">
+                    <thead>
+                        <tr>
+                            <th>Year</th>
+                            <th>Interest</th>
+                            <th>Principal</th>
+                            <th>Owing</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {tableRows}
+                    </tbody>
+                </Table>
+            </div>
         );
     }
 
